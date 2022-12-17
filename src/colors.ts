ce7eff5f6945c21de654485e486aa325a33949a8
haithamassoli <haitham.b.assoli@gmail.com>
2022-12-17 22:08:44 +0300

    app icon and splash screen

--- src/colors.ts
+++ src/colors.ts
@@ -1,7 +1,12 @@
 const Colors = {
   mainBackground: "#091032",
+  mainBackground2: "#0F1538",
   backgroundSec: "#262626",
   text: "#eee",
+  darkText: "#001438",
+  primary: "#B1A873",
+  primary2: "#FFDD88",
+  primary3: "#E1D9A8",
 };
 
 export default Colors;
